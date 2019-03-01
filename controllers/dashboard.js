df797be7a4133641914af99a744863a4323888f5
Glitch (project-tln-01) <none>
2019-03-01 09:24:58 +0000

    embed map & complete other stuff on labs

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const teamlistCollection = require('../models/teamlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
-      title: 'Dashboard - Playlist',
+      title: 'Dashboard - NBA Teams',
+      teamlists: teamlistCollection.getAllTeamlists(),
     };
+    logger.info('about to render', teamlistCollection.getAllTeamlists());
     response.render('dashboard', viewData);
   },
+  deleteTeamlist(request, response) {
+    const teamlistId = request.params.id;
+    logger.debug(`Deleting teamlist ${teamlistId}`);
+    teamlistCollection.removeTeamlist(teamlistId);
+    response.redirect('/dashboard/');
+  },
+  
 };
 
 module.exports = dashboard;
+
